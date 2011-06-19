11ab09de08344b8953198305dd9779252ea63512
Stepan Prokipchyn <korvin@Matrix.(none)>
2011-06-19 21:31:56 +0300

    Rails project created

--- Documents/brainstorm.mm
+++ Documents/brainstorm.mm
@@ -1,7 +1,7 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node BACKGROUND_COLOR="#19e06b" CREATED="1308505941100" ID="ID_1689735485" MODIFIED="1308506932746" STYLE="fork" TEXT="dobro.in.ua">
-<node CREATED="1308506254699" HGAP="79" ID="ID_73843061" MODIFIED="1308506437861" POSITION="right" STYLE="bubble" TEXT="&#x41d;&#x430;&#x437;&#x432;&#x430;" VSHIFT="-58">
+<node BACKGROUND_COLOR="#19e06b" CREATED="1308505941100" ID="ID_1689735485" MODIFIED="1308507967380" STYLE="fork" TEXT="dobro.in.ua">
+<node CREATED="1308506254699" HGAP="48" ID="ID_73843061" MODIFIED="1308507981165" POSITION="right" STYLE="bubble" TEXT="&#x41d;&#x430;&#x437;&#x432;&#x430;" VSHIFT="-201">
 <node CREATED="1308506285247" ID="ID_1499752777" MODIFIED="1308506490184" TEXT="dobro.in.ua">
 <richcontent TYPE="NOTE"><html>
   <head>
@@ -18,8 +18,8 @@
 </node>
 <node CREATED="1308506304631" ID="ID_217014913" MODIFIED="1308506412511" TEXT="help.in.ua"/>
 </node>
-<node CREATED="1308506513445" ID="ID_1773676343" MODIFIED="1308506534800" POSITION="left" STYLE="bubble" TEXT="&#x425;&#x43e;&#x441;&#x442;&#x456;&#x43d;&#x433;">
-<node CREATED="1308506541832" ID="ID_991579116" MODIFIED="1308506567197" TEXT="Heroku">
+<node CREATED="1308506513445" HGAP="66" ID="ID_1773676343" MODIFIED="1308507895516" POSITION="left" STYLE="bubble" TEXT="&#x425;&#x43e;&#x441;&#x442;&#x456;&#x43d;&#x433;" VSHIFT="-80">
+<node CREATED="1308506541832" ID="ID_991579116" MODIFIED="1308507597848" TEXT="Heroku">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -32,10 +32,23 @@
 </html>
 </richcontent>
 <icon BUILTIN="messagebox_warning"/>
+<node CREATED="1308507312239" ID="ID_917115817" MODIFIED="1308507595262" STYLE="fork" TEXT="&#x421;&#x442;&#x432;&#x43e;&#x440;&#x438;&#x442;&#x438; &#x440;&#x435;&#x43f;&#x43e;&#x437;&#x438;&#x442;&#x43e;&#x440;&#x456;&#x439;">
+<icon BUILTIN="button_ok"/>
 </node>
 </node>
-<node CREATED="1308506767504" ID="ID_1542607899" MODIFIED="1308506830064" POSITION="right" STYLE="bubble" TEXT="&#x420;&#x435;&#x43f;&#x43e;&#x437;&#x438;&#x442;&#x43e;&#x440;&#x456;&#x439;">
-<node CREATED="1308506786904" ID="ID_1370635780" MODIFIED="1308506841547" STYLE="fork" TEXT="github.com"/>
+</node>
+<node CREATED="1308506767504" HGAP="65" ID="ID_1542607899" MODIFIED="1308507961059" POSITION="left" STYLE="bubble" TEXT="&#x420;&#x435;&#x43f;&#x43e;&#x437;&#x438;&#x442;&#x43e;&#x440;&#x456;&#x439;" VSHIFT="-112">
+<node CREATED="1308506786904" ID="ID_1370635780" MODIFIED="1308508259488" STYLE="bubble" TEXT="github.com">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1308507339934" ID="ID_903781287" MODIFIED="1308507360371" STYLE="fork" TEXT="&#x421;&#x442;&#x432;&#x43e;&#x440;&#x438;&#x442;&#x438; &#x440;&#x435;&#x43f;&#x43e;&#x437;&#x438;&#x442;&#x43e;&#x440;&#x456;&#x439;">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1308507970736" HGAP="34" ID="ID_1707045352" MODIFIED="1308508041841" POSITION="right" STYLE="bubble" TEXT="&#x420;&#x43e;&#x437;&#x440;&#x43e;&#x431;&#x43a;&#x430;" VSHIFT="10">
+<node CREATED="1308508190998" ID="ID_236987822" MODIFIED="1308508266518" STYLE="fork" TEXT="&#x421;&#x442;&#x432;&#x43e;&#x440;&#x435;&#x43d;&#x43d;&#x44f; &#x43f;&#x440;&#x43e;&#x435;&#x43a;&#x442;&#x443;">
+<icon BUILTIN="button_ok"/>
+</node>
 </node>
 </node>
 </map>
---
+++ dobro.in.ua/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DobroInUa::Application.config.secret_token = 'cb9b3f9c26ee0953a6e31e9b77cceffe8978288ebd402669e57d02a3c4b827044e9d1f067731d50eaf0b4ea7b7883b184effe2b2aeb79797d554cef07c279782'
