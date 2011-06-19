3f497000f8c3bd4eb35c6a9a7eb2f1b86b9d0541
Stepan Prokipchyn <korvin@Matrix.(none)>
2011-06-19 21:42:39 +0300

    Project created correctly

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DobroInUa::Application.config.secret_token = 'eb31d2eee6f25ce64ec418407f906b29e665af013267cde24a3e4a018ecbd271b679591c010d57faeecac096d1784fa1eb6f48dca3c45cc481859998a844ef5c'
