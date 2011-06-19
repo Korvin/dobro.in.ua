9666579147e007ee3c82b850346c83a254992306
Stepan Prokipchyn <korvin@Matrix.(none)>
2011-06-19 21:12:26 +0300

    Project started

---
+++ Documents/brainstorm.mm
@@ -0,0 +1,41 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#19e06b" CREATED="1308505941100" ID="ID_1689735485" MODIFIED="1308506932746" STYLE="fork" TEXT="dobro.in.ua">
+<node CREATED="1308506254699" HGAP="79" ID="ID_73843061" MODIFIED="1308506437861" POSITION="right" STYLE="bubble" TEXT="&#x41d;&#x430;&#x437;&#x432;&#x430;" VSHIFT="-58">
+<node CREATED="1308506285247" ID="ID_1499752777" MODIFIED="1308506490184" TEXT="dobro.in.ua">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1055;&#1086;&#1090;&#1086;&#1095;&#1085;&#1080;&#1081; &#1074;&#1072;&#1088;&#1110;&#1072;&#1085;&#1090;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1308506304631" ID="ID_217014913" MODIFIED="1308506412511" TEXT="help.in.ua"/>
+</node>
+<node CREATED="1308506513445" ID="ID_1773676343" MODIFIED="1308506534800" POSITION="left" STYLE="bubble" TEXT="&#x425;&#x43e;&#x441;&#x442;&#x456;&#x43d;&#x433;">
+<node CREATED="1308506541832" ID="ID_991579116" MODIFIED="1308506567197" TEXT="Heroku">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1055;&#1054;&#1090;&#1086;&#1095;&#1085;&#1080;&#1081; &#1074;&#1072;&#1088;&#1110;&#1072;&#1085;&#1090;
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1308506767504" ID="ID_1542607899" MODIFIED="1308506830064" POSITION="right" STYLE="bubble" TEXT="&#x420;&#x435;&#x43f;&#x43e;&#x437;&#x438;&#x442;&#x43e;&#x440;&#x456;&#x439;">
+<node CREATED="1308506786904" ID="ID_1370635780" MODIFIED="1308506841547" STYLE="fork" TEXT="github.com"/>
+</node>
+</node>
+</map>
